c632c04c687b0911636105f828fc2eff472ff6db
enock <enckkipronoh@gmail.com>
2021-10-17 14:03:46 +0300

    styling the profile page

--- instagram/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from instagram.models import Post, Comment
-
-admin.site.register(Post)
-admin.site.register(Comment)
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,13 +1,23 @@
-from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from . import views
+"""instagram URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import include, url
+from django.contrib.auth import views 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='instagram-home'),
-    path('about/',views.about, name='instagram-about'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('<int:pk>',views.like, name='like'),
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('gram.urls')),
 ]
